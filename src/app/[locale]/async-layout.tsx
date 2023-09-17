8e6add86cf819f69dc616dd708a27d189b302ce7
Rolando Andrade <rolandoandradefernandez@gmail.com>
2023-09-16 20:09:38 -0400

    Extracted uselocale from async component

---
+++ src/app/[locale]/async-layout.tsx
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl';
+import { Navbar } from '@/components/navbar';
+
+type AsyncLayoutProps = {
+  locale: string;
+  children: React.ReactNode;
+}
+
+export async function AsyncLayout({locale, children}: AsyncLayoutProps) {
+  let messages;
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default
+  } catch (error) {
+    notFound();
+  }
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <Navbar></Navbar>
+      {children}
+    </NextIntlClientProvider>
+  )
+}
